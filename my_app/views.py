a8140f94e6acce873675c50997a39605ac727ad0
Alex-dot-stack <schlee_alexander@gmx.net>
2023-02-14 20:27:03 +0100

    Add frontend and requirements

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(request):
     return render(request, template_name='base.html')
+
+def new_search(request):
+    # pull data from search bar - was wir eintippen möchten wir auffangen
+    search = request.POST.get('search') # das ist ein Python Dictionary get(für extrahieren) - weil POST Method dictionary liefert
+    print(search) # double check in console ob wirklich ausgegeben wird wonach du gesucht hast
+    # hier übergeben wir gleich werte für front-end
+    stuff_for_front_end = {
+        'search': search
+    }
+    return render(request, 'myapp/new_search.html', stuff_for_front_end)
